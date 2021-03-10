a4fc12e8ac36e842acf7942eae5304956365d0db
Nico Hahn <nico@f-hahn.de>
2021-03-10 16:42:42 +0100

    model selection germany

--- R/model_selection_germany.R
+++ R/model_selection_germany.R
@@ -10,12 +10,28 @@ library(randomForestSRC)
 library(spdep)
 start <- Sys.time()
 source("R/preprocess_germany.R")
-start <- Sys.time()
+start <- Sys.time()                                 
 parallelMap::parallelStartSocket(7)
 set.seed(420)
-sel <- INLAModelSel(
+sel_all <- INLAModelSel(
   "CumNumberTestedIll",
   colnames(newest_numbers)[c(2:5, 8:18, 26:40, 43:48, 56:58, 60)],
+  "idarea_1",
+  "iid",
+  "nbinomial",
+  newest_numbers
+)
+sel_demo <- INLAModelSel(
+  "CumNumberTestedIll",
+  colnames(newest_numbers)[c(2:5, 8:18, 56:58)],
+  "idarea_1",
+  "iid",
+  "nbinomial",
+  newest_numbers
+)
+sel_infra <- INLAModelSel(
+  "CumNumberTestedIll",
+  colnames(newest_numbers)[c(26:40, 43:48, 56:57, 60)],
   "idarea_1",
   "iid",
   "nbinomial",
