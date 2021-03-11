feec8774b8883f65aa9674650e69ee4539e7aaa1
Nico Hahn <nico@capia.no>
2021-03-11 09:54:18 +0100

    model selection for norway

--- R/model_selection_norge.R
+++ R/model_selection_norge.R
@@ -9,7 +9,77 @@ library(mlr)
 library(randomForestSRC)
 library(spdep)
 source("R/preprocess_norge.R")
-parallelMap::parallelStartSocket(6)
+parallelMap::parallelStartSocket(7)
+newest_numbers$geometry <- NULL
+stack_all <- inla.stack(
+  data = list(value = newest_numbers$value),
+  A = list(1),
+  effects = list(
+    data.frame(
+      Intercept = 1,
+      newest_numbers[, c(7:51, 53, 60:62)]
+    )
+  )
+)
+stack_demo <- inla.stack(
+  data = list(value = newest_numbers$value),
+  A = list(1),
+  effects = list(
+    data.frame(
+      Intercept = 1,
+      newest_numbers[, c(7:30, 60:62)]
+    )
+  )
+)
+stack_infra <- inla.stack(
+  data = list(value = newest_numbers$value),
+  A = list(1),
+  effects = list(
+    data.frame(
+      Intercept = 1,
+      newest_numbers[, c(37:51, 53, 60, 61)]
+    )
+  )
+)
+result_all <- INLAstep(
+  fam1 = "nbinomial",
+  newest_numbers,
+  in_stack = stack_all,
+  invariant = "0 + Intercept",
+  direction = "backwards",
+  include = c(7:51, 53, 60:62),
+  y = "value",
+  y2 = "value",
+  powerl = 1,
+  inter = 1,
+  thresh = 2
+)
+result_demo <- INLAstep(
+  fam1 = "nbinomial",
+  newest_numbers,
+  in_stack = stack_demo,
+  invariant = "0 + Intercept",
+  direction = "backwards",
+  include = c(7:30, 60:62),
+  y = "value",
+  y2 = "value",
+  powerl = 1,
+  inter = 1,
+  thresh = 2
+)
+result_infra <- INLAstep(
+  fam1 = "nbinomial",
+  newest_numbers,
+  in_stack = stack_infra,
+  invariant = "0 + Intercept",
+  direction = "backwards",
+  include = c(37:51, 53, 60, 61),
+  y = "value",
+  y2 = "value",
+  powerl = 1,
+  inter = 1,
+  thresh = 2
+)
 set.seed(420)
 sel_all <- INLAModelSel(
   "value",
